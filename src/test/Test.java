6d169e3e26525a72e467a23bd90c86a999a38389
wuyasan <youtosan0@gmail.com>
2022-09-16 12:21:23 -0700

    Generate Array
    Generate a random array based on the length provided by user and print the unsorted version of it.

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,10 +1,42 @@
 package test;
 
+import java.util.*;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int length = generateLength();
+		int[] a = new int[length];
+		
+		for(int i = 0; i < length; i++)
+			a[i] = generateRand();
+		
+		System.out.print("Unsorted: ");
+		printArray(a);
 	}
 
+	public static int generateLength() {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter the length of the array: ");
+		
+		int length = input.nextInt();
+		
+		return length;
+	}
+	
+	public static int generateRand() {
+		int rand = (int)(Math.random()*100);
+		
+		return rand;
+	}
+	
+	public static void printArray(int a[]) {
+		for(int i = 0; i < a.length; i++)
+			System.out.print(a[i] + " ");
+		
+		System.out.println();
+	}
 }
